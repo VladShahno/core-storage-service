972aa4adbc8eff2cb400d07834d0365b2272f0d1
Vladyslav Shakhno <vladyslav.shakhno@cengage.com>
2024-03-09 12:54:02 +0200

    01 - initial commit

---
+++ file-store-sdk/src/main/java/com/demo/filestoresdk/FileStoreSdkApplication.java
@@ -0,0 +1,13 @@
+package com.demo.filestoresdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStoreSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStoreSdkApplication.class, args);
+	}
+
+}
---
+++ file-store-sdk/src/test/java/com/demo/filestoresdk/FileStoreSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.filestoresdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoreSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
